e21f4b3f8a186228fb83c23234bf0fa77fab2f60
Ranny Meier <Ranny@RespMech.com>
2021-09-14 12:11:42 -0500

    Added Entity and Item data

--- Data/EntSelService.cs
+++ Data/EntSelService.cs
@@ -4,19 +4,43 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Text.Json;
 using System.IO;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Http;
 
 namespace InputSelectFilter.Data
 {
     public class EntSelService
     {
-        
-
-        public List<EntSel> GetEnts()
+        private readonly DataConfig _jsonPaths;
+        private static readonly HttpClient client = new HttpClient();
+        public EntSelService(IOptions<DataConfig> options)
         {
-            string entsJson = File.ReadAllText(@"TestEnts.json");
-            var ents = JsonSerializer.Deserialize<EntList>(entsJson);
+            _jsonPaths = options.Value;
+        }
+        public async Task<List<EntSel>> GetEntsAsync()
+        {
+            HttpResponseMessage response = await client.GetAsync(_jsonPaths.EntsJsonPath);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            var ents = JsonSerializer.Deserialize<EntList>(responseBody);
             return ents.Ents;
         }
 
+        public async Task<List<EntSel>> GetItmsAsync()
+        {
+            HttpResponseMessage response = await client.GetAsync(_jsonPaths.ItmsJsonPath);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            var itms = JsonSerializer.Deserialize<EntList>(responseBody);
+            return itms.Ents;
+        }
+    }
+
+    public class DataConfig
+    {
+        public const string DataPaths = "DataPaths";
+        public string EntsJsonPath { get; set; }
+        public string ItmsJsonPath { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -30,6 +30,7 @@ namespace InputSelectFilter
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<EntSelService>();
+            services.Configure<DataConfig>(Configuration.GetSection(DataConfig.DataPaths));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
