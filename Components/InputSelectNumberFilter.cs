03ac45c665187fbc64158f07a07869a853f4bd92
Ranny Meier <Ranny@RespMech.com>
2021-09-15 11:59:25 -0500

    InputSelectNumberFilter

---
+++ Components/InputSelectNumberFilter.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InputSelectFilter.Components
+{
+    public class InputSelectNumberFilter<T> : InputSelect<T>
+    {
+        //TBD
+    }
+}
